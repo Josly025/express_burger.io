f0639b56a8668a20d0f09abca6e69910c8922836
Grantjos12 <grantjoslyn@gmail.com>
2020-10-28 22:15:29 -0500

    connected to sql burgers_db

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,23 @@
 //connect node to sql
 //export the connection
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Grantjos12",
+  database: "burgers_db",
+});
+
+//using connection variable to connect
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+//export for sql connection
+module.exports = connection;
