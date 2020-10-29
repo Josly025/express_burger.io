2a6a452b00380a161feea80b35b42ef361f343e9
Grantjos12 <grantjoslyn@gmail.com>
2020-10-28 20:57:01 -0500

    added starter files and populated schema & seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Queries to populate burgers table 
+-- At least 3 entries
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Little Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("HUGE burger", true);
